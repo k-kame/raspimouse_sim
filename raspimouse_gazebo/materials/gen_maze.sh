bbfef82c083aa989aec40726b7e38f690cfceb3f
b_donta <kentaro.kameyama@gmail.com>
2020-09-13 09:50:18 +0900

    迷路を変更

--- raspimouse_gazebo/materials/gen_maze.sh
+++ raspimouse_gazebo/materials/gen_maze.sh
@@ -2,4 +2,5 @@
 
 WORKINGDIR="$HOME/catkin_ws/src/raspimouse_sim/raspimouse_gazebo"
 
-rosrun xacro xacro.py $WORKINGDIR/materials/sample_maze.world.xacro > $WORKINGDIR/worlds/sample_maze.world
+# rosrun xacro xacro.py $WORKINGDIR/materials/sample_maze.world.xacro > $WORKINGDIR/worlds/sample_maze.world
+rosrun xacro xacro $WORKINGDIR/materials/sample_maze.world.xacro > $WORKINGDIR/worlds/sample_maze.world
